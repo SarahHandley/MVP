392b9bdd1a8de08be372038f9a728b7fb3728e9d
Sarah Handley <handleysarah99@gmail.com>
2022-11-14 15:31:58 -0600

    setup initial client and server

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static(__dirname + '/../client/dist'));
+
+// add routes here
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+
+module.exports = {
+    mode: "development",
+    entry: path.join(__dirname, "/client/src/index.jsx"),
+    output: {
+        path: path.join(__dirname, '/client/dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /nodeModules/,
+          use: {
+            loader: "babel-loader",
+          },
+        }
+      ]
+    },
+    devtool: 'source-map'
+};
