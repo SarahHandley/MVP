39dfe8e2a523fa870cc74f49e6c7b1ebdc5b62cb
Sarah Handley <handleysarah99@gmail.com>
2022-11-14 19:36:30 -0600

    start writing get songs request

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import axios from 'axios';
+
+const { useState, useEffect } = React;
 
 const App = () => {
-  return (<div>Hello World</div>);
+  const [songs, setSongs] = useState([]);
+  const [photo, setPhoto] = useState('');
+
+  const getSongs = () => {
+    return axios.get('http://acnhapi.com/v1/songs')
+      .then((response) => console.log(response.data))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getSongs();
+  }, [songs]);
+
+  return (
+  <div>Hello World {console.log(songs)}</div>);
 };
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const axios = require('axios');
 const app = express();
 const PORT = 3000;
 
@@ -7,8 +7,6 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + '/../client/dist'));
-
-// add routes here
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
