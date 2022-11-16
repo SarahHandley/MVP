5009b0922af0d5f143529986cd1afb736b07935b
Sarah Handley <handleysarah99@gmail.com>
2022-11-16 16:56:02 -0600

    add pagination for better viewing

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -10,6 +10,7 @@ const { useState, useEffect } = React;
 const App = () => {
   const [email, setEmail] = useState('');
   const [songs, setSongs] = useState([]);
+  const [page, setPage] = useState(0);
   const [selectedSongs, setSelectedSongs] = useState([]);
   const [currentSong, setCurrentSong] = useState([]);
   const [userId, setUserId] = useState(0);
@@ -24,7 +25,7 @@ const App = () => {
   };
 
   const handleClickAlbum = (songId) => {
-    setCurrentSong(songs[songId]);
+    setCurrentSong(songs[(12 * page) + songId]);
     if (currentSong.length !== 0) {
       let audioPlaying = document.getElementsByClassName('audio');
       audioPlaying[0].pause();
@@ -41,7 +42,8 @@ const App = () => {
     }
   };
 
-  const addSongToSelected = (songId) => {
+  const addSongToSelected = (id) => {
+    let songId = (12 * page) + id;
     if (!songs[songId][3]) {
       let newSelectedSong = songs[songId].slice();
       newSelectedSong.splice(3, 1, true);
@@ -107,7 +109,7 @@ const App = () => {
           <img id='isabelle-photo' src='isabelle_jammin.jpeg' height='250px'></img>
         </div>
         <div id='songs-container'>
-          <SongList songs={songs} addSongToSelected={addSongToSelected} handleClickAlbum={handleClickAlbum}/>
+          <SongList songs={songs} page={page} setPage={setPage} addSongToSelected={addSongToSelected} handleClickAlbum={handleClickAlbum}/>
           <div id='right-songs-container'>
             <SelectedSongList selectedSongs={selectedSongs} removeSongFromSelected={removeSongFromSelected} handleClickPlay={handleClickPlay}/>
             {currentSong.length !== 0 &&
@@ -115,10 +117,6 @@ const App = () => {
             }
           </div>
         </div>
-        <audio controls className='audio'>
-          <source src='https://dodo.ac/np/images/0/04/NH_Agent_K.K._%28Aircheck%2C_Hi-Fi%29.flac' type='audio/mp3'></source>
-          Your browser does not support audio.
-        </audio>
       </div>
     }
   </div>);
--- client/src/components/SongList.jsx
+++ client/src/components/SongList.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import SongListItem from './SongListItem.jsx';
 
-const SongList = ({ songs, addSongToSelected, handleClickAlbum }) => {
+const SongList = ({ songs, page, setPage, addSongToSelected, handleClickAlbum }) => {
+  const handleLeftArrowClick = () => {
+    if (page !== 0) {
+      setPage(page - 1);
+    }
+  };
+
+  const handleRightArrowClick = () => {
+    if (page !== 8) {
+      setPage(page + 1);
+    }
+  };
+
   return(
     <div className='song-list-container'>
       <h2 className='subtitle'> All Songs </h2>
       <div id='song-list'>
-        {songs.map((song, i) => {
+        {songs.slice(12 * page, 12 * (page + 1)).map((song, i) => {
           return(<SongListItem song={song} key={i} addSongToSelected={addSongToSelected} handleClickAlbum={handleClickAlbum} id={i}/>)
         })}
+      </div>
+      <div id='arrow-container'>
+        <i className="fa-solid fa-arrow-left" onClick={handleLeftArrowClick}></i>
+        <i className="fa-solid fa-arrow-right" onClick={handleRightArrowClick}></i>
       </div>
     </div>
   );
